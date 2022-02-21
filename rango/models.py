a787d3390f3cb507548fe83f54957d367e9891cf
Xinheng Lyu <2642528L@student.gla.ac.uk>
2022-02-21 01:47:54 +0000

    Chapter9

--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,7 @@
 from django import views
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -31,6 +32,19 @@ class Page(models.Model):
     views = models.IntegerField(default=0)
     def __str__(self):
        return self.title
+       
+class UserProfile(models.Model):
 
+# This line is required. Links UserProfile to a User model instance. 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+# The additional attributes we wish to include. 
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 slug = models.SlugField(unique=True)
+
+
+
